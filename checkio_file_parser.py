0810289db1255301d76e6213d1cda4c005829bd1
Oleksandr Zozulia <freemanlex@gmail.com>
2022-08-14 14:04:27 +0300

    to list conversion commented lines

--- checkio_file_parser.py
+++ checkio_file_parser.py
@@ -187,6 +187,7 @@ referee_py = open(f"{directory_name}\\{mission_name}\\verification\\referee.py",
 referee_py.write('''from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io_template import CheckiOReferee
+# from checkio.referees.checkers import to_list
 
 from tests import TESTS
 
@@ -194,6 +195,7 @@ api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
+        # checker=to_list,
         function_name={
             "python":"''' + func_name + '''",
             "js": "''' + js_func_name + '''"
